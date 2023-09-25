477e77eedda295331bdc01f9a90fea859bdd1bcb
neobarbarian <32205741+neobarbarian@users.noreply.github.com>
2023-09-25 10:01:28 -0500

    supress devtools on open

--- src/main/main.ts
+++ src/main/main.ts
@@ -50,8 +50,8 @@ if (process.env.NODE_ENV === 'production') {
   sourceMapSupport.install();
 }
 
-const isDebug =
-  process.env.NODE_ENV === 'development' || process.env.DEBUG_PROD === 'true';
+const isDebug = false;
+// process.env.NODE_ENV === 'development' || process.env.DEBUG_PROD === 'true';
 
 if (isDebug) {
   require('electron-debug')();
